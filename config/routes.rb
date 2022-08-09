be7fd0a31fb40c4deaad6d53d2d1281970f6f815
Alison Jeffrey <alisonnwebb@hotmail.com>
2022-08-09 15:22:29 +0900

    Skeleton complete without styling or refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # root to: 'tasks#index'
+
+  # Read all aka list all tasks
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # Create a new task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # Read one aka show one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Update a single task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Destroy aka delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
